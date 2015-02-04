96042ef147b1b23b44b040104049caf91159f8bd
Daniel Chen <Maveri2k@yahoo.com>
2015-02-04 14:47:28 -0500

    setup.py file

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='sphinx-rtd-test',
+    author='Daniel Chen',
+    description='testing sphinx documentation with read the docs',
+    packages=['sphinx-rtd'],
+    platforms='any',
+    classifiers=[
+        'Programming Language :: Python',
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        ],
+)
