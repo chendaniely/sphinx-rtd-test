1179651988779b70f9015e98e88e7325981fff15
Daniel Chen <Maveri2k@yahoo.com>
2015-02-04 14:36:05 -0500

    init and my_square function with documentation

---
+++ sphinx-rtd/mod.py
@@ -0,0 +1,12 @@
+""" Test module and some documentation
+"""
+
+
+def my_square(x):
+    """Square a given value
+
+    :param x: value to be squared
+    :type x: a number
+    :rtype: float
+    """
+    return x ** 2
